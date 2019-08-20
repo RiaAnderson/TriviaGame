ac9c34c1c937336e1d13f58688a9892fab808973
Ria <astoriaa92@gmail.com>
2019-08-20 16:49:25 -0700

    Added js pseudocode and variables.

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+//variables 
+var question = "";
+var answer = "";
+var timer = "";
+var totalScore = 0;
+var rightScore = 0;
+var wrongScore = 0;
+//count down timer
+//function to select answer
+//provide a score at the end of the game
+//correct answers score
+//incorrect answers score
+//if times out end the game and provide scores
+//restart game function
